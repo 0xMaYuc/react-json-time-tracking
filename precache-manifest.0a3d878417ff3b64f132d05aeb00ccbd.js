cb771391ca6d38ae3de4f58c0c94b33b221f7d4d
SK <0xmayuc@protonmail.com>
2022-09-09 11:04:09 -0500

    Updates

---
+++ precache-manifest.0a3d878417ff3b64f132d05aeb00ccbd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fca9b9c18f4a2d184897",
+    "url": "/react-json-time-tracking/static/css/main.29b3e94c.chunk.css"
+  },
+  {
+    "revision": "fca9b9c18f4a2d184897",
+    "url": "/react-json-time-tracking/static/js/main.fca9b9c1.chunk.js"
+  },
+  {
+    "revision": "4a11c094a067ca26ee16",
+    "url": "/react-json-time-tracking/static/css/1.dff88e91.chunk.css"
+  },
+  {
+    "revision": "4a11c094a067ca26ee16",
+    "url": "/react-json-time-tracking/static/js/1.4a11c094.chunk.js"
+  },
+  {
+    "revision": "2f1f34e133f9ef933954",
+    "url": "/react-json-time-tracking/static/js/runtime~main.2f1f34e1.js"
+  },
+  {
+    "revision": "b166cda623a8a7acdde0ffd51667229c",
+    "url": "/react-json-time-tracking/static/media/icon-work.b166cda6.svg"
+  },
+  {
+    "revision": "1a5250a3891fdf036f44c0141a2407d9",
+    "url": "/react-json-time-tracking/static/media/icon-play.1a5250a3.svg"
+  },
+  {
+    "revision": "267a0ddfeba20d0ffb9ac1c4e2b58aca",
+    "url": "/react-json-time-tracking/static/media/icon-study.267a0ddf.svg"
+  },
+  {
+    "revision": "0572c3afee42a4eaaa86e6e785af5339",
+    "url": "/react-json-time-tracking/static/media/icon-social.0572c3af.svg"
+  },
+  {
+    "revision": "1f2aa98ef1bebee3fa976340de755146",
+    "url": "/react-json-time-tracking/static/media/icon-exercise.1f2aa98e.svg"
+  },
+  {
+    "revision": "78d6a4ccd44b9c9c8a8512f9bbbb70ee",
+    "url": "/react-json-time-tracking/static/media/icon-self-care.78d6a4cc.svg"
+  },
+  {
+    "revision": "59e2fc85216d4075f2397f8fd7bc0520",
+    "url": "/react-json-time-tracking/index.html"
+  }
+];
